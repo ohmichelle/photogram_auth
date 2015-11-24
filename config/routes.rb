efaf95497da01501bf315405faf8afc43cf501ae
ohmichelle <oh.michelles@gmail.com>
2015-11-23 21:05:45 -0600

    users rcav, to step 15

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,13 @@ Rails.application.routes.draw do
   #------------------------------
 
   devise_for :users
+
+  # Routes for the User resource:
+  # READ
+  get "/users", :controller => "users", :action => "index"
+  get "/users/:id", :controller => "users", :action => "show"
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
