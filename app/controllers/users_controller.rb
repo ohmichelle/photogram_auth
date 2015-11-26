63a1f6485f4e3a855595b8e936ee035b41fae05a
ohmichelle <oh.michelles@gmail.com>
2015-11-26 03:02:26 -0600

    formatting, convert index to instagram view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @photos = Photo.all
+    @comment = Comment.new
   end
 
   def my_likes
     @user_likes = current_user.liked_photos
+    @photos = Photo.all
+    @comment = Comment.new
     #@photos = Photo.all
   end
 end
